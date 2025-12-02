1875c4117e93cc1a486e3da496163a44bce379ee
kilyong jeong <uzoolove@gmail.com>
2025-12-02 15:50:54 +0900

    10 Button 컴포넌트에 Props 전달 개발중.

---
+++ workspace-ins/ch02-start/props/10/src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Counter(){
+
+  const [ count, setCount ] = useState(0);
+
+  return (
+    <div id="counter">
+      <button type="button">-_-</button>
+      <button type="button">0_0</button>
+      <button type="button">+_+</button>
+      <span>0</span>
+    </div>
+  );
+}
