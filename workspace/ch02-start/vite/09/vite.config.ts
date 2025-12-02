f8e7089b95f6ab7240122bc03e3c29dc1df47145
UJin1122 <enooooo93@gmail.com>
2025-12-02 14:13:22 +0900

    Vite

---
+++ workspace/ch02-start/vite/09/vite.config.ts
@@ -0,0 +1,14 @@
+// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+    ],
+  },
+})
