8c94950b4e3a02d0f2777a17b87d1d0de9a93bda
kilyong jeong <uzoolove@gmail.com>
2025-12-03 10:00:47 +0900

    10 Button 컴포넌트에 Props 전달 작성중.

--- workspace-ins/ch02-start/props/10/src/components/Counter.tsx
+++ workspace-ins/ch02-start/props/10/src/components/Counter.tsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
 
 export default function Counter(){
+  console.log('\tCounter 렌더링.');
 
   const [ count, setCount ] = useState(0);
+
+  const countUp = () => {
+    setCount(count + 1);
+  };
+
+  function countDown(){
+    setCount(count - 1);
+  }
+
+  const reset = () => {
+    setCount(0);
+  };
 
   return (
     <div id="counter">
       <button type="button">-_-</button>
       <button type="button">0_0</button>
       <button type="button">+_+</button>
-      <span>0</span>
+      <span>{ count }</span>
     </div>
   );
 }
