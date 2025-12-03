80d5b0993a9cf36f8d23bc7fd404f167e1ea55f1
kilyong jeong <uzoolove@gmail.com>
2025-12-03 10:47:26 +0900

    10 Button 컴포넌트에 Props 전달 완성

--- workspace-ins/ch02-start/props/10/src/components/Button.tsx
+++ workspace-ins/ch02-start/props/10/src/components/Button.tsx
@@ -2,17 +2,19 @@ interface ButtonProps{
   children: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   type?: 'button' | 'submit' | 'reset';
+  color?: 'red' | 'gray' | 'green';
 }
 
 // children prop은 특별히 예약된 이름으로, 
 // 컴포넌트 사이에 넣는 모든 요소나 텍스트가 자동으로 전달됨
-function Button({ children, onClick, type='button' }: ButtonProps){
+function Button({ children, onClick, type='button', color="green" }: ButtonProps){
   console.log('\t\tButton 렌더링.');
   return (
     <button 
       type={ type } 
       onClick={ onClick }
       className="rounded-button"
+      style={{ backgroundColor: color }}
     >
       { children }
     </button>
