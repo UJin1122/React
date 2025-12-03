2bf99143899830a4c12fe3eccc49f4d9221076e9
kilyong jeong <uzoolove@gmail.com>
2025-12-03 10:47:26 +0900

    10 Button 컴포넌트에 Props 전달 완성

--- workspace-ins/ch02-start/props/10/src/components/Counter.tsx
+++ workspace-ins/ch02-start/props/10/src/components/Counter.tsx
@@ -20,8 +20,8 @@ export default function Counter(){
 
   return (
     <div id="counter">
-      <Button type="submit" onClick={ countDown }>-_-</Button>
-      <Button>0_0</Button>
+      <Button type="submit" onClick={ countDown } color="red">-_-</Button>
+      <Button color="gray">0_0</Button>
       <Button onClick={ countUp }>+_+</Button>
       <span>{ count }</span>
     </div>
