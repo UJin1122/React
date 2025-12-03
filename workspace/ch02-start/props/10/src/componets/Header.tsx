f1c8ca4861b811c8d9952b94e48a198a6bc93297
UJin1122 <enooooo93@gmail.com>
2025-12-03 09:10:44 +0900

    가운데 정렬 추가

--- workspace/ch02-start/props/10/src/componets/Header.tsx
+++ workspace/ch02-start/props/10/src/componets/Header.tsx
@@ -1,12 +1,12 @@
 function Header({title}:{title:string}){
       return (
         <header>
-          <h1>{title}</h1>
-         <div style={{ display:"flex" ,justifyContent:"center", gap:"10px" }}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYSkMo_wH4ZXCN9kV4MJ3Y-VRPN0-8mB1MyA&s"  style={{ borderRadius: "10%", width: 100, height: 100 }}/>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUflx-qdZp856QodYn517Zw8vESugvFYfgA&s"  style={{ borderRadius: "10%", width: 100, height: 100 }}/>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz__Dmsi9QBs2xO7dEyJPF0DKGuXkH0NyYAQ&s" style={{ borderRadius: "10%", width: 100, height: 100 }} />
-          </div>
+          <h1 style={{ display:"flex", justifyContent:"center" }}>{title}</h1>
+            <div style={{ display:"flex" ,justifyContent:"center", gap:"10px", padding:"10px" }}>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYSkMo_wH4ZXCN9kV4MJ3Y-VRPN0-8mB1MyA&s"  style={{ borderRadius: "10%", width: 100, height: 100 }}/>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEUflx-qdZp856QodYn517Zw8vESugvFYfgA&s"  style={{ borderRadius: "10%", width: 100, height: 100 }}/>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz__Dmsi9QBs2xO7dEyJPF0DKGuXkH0NyYAQ&s" style={{ borderRadius: "10%", width: 100, height: 100 }} />
+            </div>
         </header>
       );
 }
