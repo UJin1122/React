949c502ae688801bafd58bd8bec73ac95ddbe7a8
kilyong jeong <uzoolove@gmail.com>
2025-12-03 09:44:28 +0900

    .

--- workspace-ins/ch02-start/vite/09/src/pages/TodoList.tsx
+++ workspace-ins/ch02-start/vite/09/src/pages/TodoList.tsx
@@ -4,6 +4,9 @@ interface TodoListProps{
 }
 
 function TodoList({ list }: TodoListProps){
+  // const items = list.map(function(item){
+  //   return <li key={ item._id }>{ item.title } - { item.done ? '완료' : '진행중' }</li>;
+  // });
   const items = list.map(item => <li key={ item._id }>{ item.title } - { item.done ? '완료' : '진행중' }</li>);
   return (
     <ul className="todolist">
