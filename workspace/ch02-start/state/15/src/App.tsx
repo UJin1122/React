382c40f881f0a2ff2dc09e934456734f598f0d38
UJin1122 <enooooo93@gmail.com>
2025-12-05 15:22:42 +0900

    state 15

--- workspace/ch02-start/state/15/src/App.tsx
+++ workspace/ch02-start/state/15/src/App.tsx
@@ -2,164 +2,169 @@ import { useState } from 'react';
 import './App.css';
 
 // 이메일 검증 정규식
-  const emailExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const emailExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 // 휴대폰 검증 정규식
 const cellphoneExp = /^010[0-9]{4}[0-9]{4}$/;
 
-interface Member{
-    name: string;
-    email: string;
-    cellphone: string;
+interface Member {
+  name: string;
+  email: string;
+  cellphone: string;
 }
 
-interface FromErrors {
+interface FormErrors {
   name?: { message: string };
   email?: { message: string };
   cellphone?: { message: string };
 }
 
 interface ValidateOptions {
-  criteriaMode? : 'firstError' | 'all'
+  criteriaMode?: 'firstError' | 'all';
+  data?: Member;
 }
 
-function App(){
-  // const [ name, setName ] = useState('');
-  // const [ email, setEmail ] = useState('');
-  // const [ cellphone, setCellphone ] = useState('010');
-  
-  // const user: Member ={
-  //   name,
-  //   email,
-  //   cellphone
-  // };
-  
-  // function handleNameChange(event: React.ChangeEvent<HTMLInputElement>){
-  //   setName(event.target.value);
-  // }
-  // function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>){
-  //   setEmail(event.target.value);
-  // } 
-  // function handleCellPhoneChange(event: React.ChangeEvent<HTMLInputElement>){
-  //   setCellphone(event.target.value);
-  // }
+function App() {
+  console.log('App 렌더링');
+
+  // input 요소를 제어 컴포넌트로 만들기 위해서 상태로 정의
+  /*
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ cellphone, setCellphone ] = useState('010');
+
+  const user: Member = {
+    name,
+    email,
+    cellphone
+  };
+
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+  function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
+  }
+  function handleCellphoneChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setCellphone(event.target.value);
+  }
+  */
 
   const [ user, setUser ] = useState<Member>({
-    name:'',
-    email:'',
-    cellphone:'010'
+    name: '',
+    email: '',
+    cellphone: '010'
   });
 
-  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>){
-    setUser({
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newUser = {
       ...user,
-      [event.target.name]: event.target.value
-    });
+      [event.target.name]: event.target.value,
+    };
+    setUser(newUser);
+    validate({ data: newUser });
   }
+
+  const [ errors , setErrors ] = useState<FormErrors>({});
+
   
-  const [ errors, setErrors ] = useState<FromErrors>({});
+  function validate({ criteriaMode = 'firstError', data }: ValidateOptions = {}){
+    const userInfo = data || user;
 
-  function validate(options: ValidateOptions){
-    const newErrors: FromErrors = {};
-    //   // 필수 입력 체크
-    //   if(user.name.trim() === ''){
-    //     newErrors = {
-    //       name: { message: '이름을 입력하세요.' }
-    //     };
-    //   }else if(user.name.trim().length < 2){
-    //     newErrors = {
-    //       name: { message: '2글자 이상 입력하세요.' }
-    //     };
-    //   }else if(user.email.trim() === ''){
-    //     newErrors = {
-    //       email: { message: '이메일을 입력하세요.' }
-    //     };
-    //   }else if(emailExp.test(user.email) === false){
-    //     newErrors = {
-    //       email: { message: '이메일 양식에 맞지 않습니다.' }
-    //     };
-    //   }else if(user.cellphone.trim() === ''){
-    //     newErrors = {
-    //       cellphone: { message: '휴대폰 번호를 입력하세요.' }
-    //     };
-    //   }else if(cellphoneExp.test(user.cellphone) === false){
-    //     newErrors = {
-    //       cellphone: { message: '휴대폰 형식에 맞지 않습니다.' }
-    //     };
-    //   }
-    //   setErrors(newErrors);
-      
-     
-    if (options.criteriaMode === "all") {
-      
-      if (user.name.trim() === "") {
-        newErrors.name = { message: "이름을 입력하세요." };
-      } else if (user.name.trim().length < 2) {
-        newErrors.name = { message: "2글자 이상 입력하세요." };
+    const newErrors: FormErrors = {};
+
+    // 필수 입력 체크
+    if(userInfo.name.trim() === ''){
+      newErrors.name = { message: '이름을 입력하세요.' };
+      if (criteriaMode === 'firstError') {
+        setErrors(newErrors);
+        return;
       }
-
-      if (user.email.trim() === "") {
-        newErrors.email = { message: "이메일을 입력하세요." };
-      } else if (emailExp.test(user.email) === false) {
-        newErrors.email = { message: "이메일 양식에 맞지 않습니다." };
-      }
-
-      if (user.cellphone.trim() === "") {
-        newErrors.cellphone = { message: "휴대폰 번호를 입력하세요." };
-      } else if (cellphoneExp.test(user.cellphone) === false) {
-        newErrors.cellphone = { message: "휴대폰 형식에 맞지 않습니다." };
+    }else if(userInfo.name.trim().length < 2){
+      newErrors.name = { message: '2글자 이상 입력하세요.' };
+      if (criteriaMode === 'firstError') {
+        setErrors(newErrors);
+        return;
       }
     }
-
+    
+    if(userInfo.email.trim() === ''){
+      newErrors.email = { message: '이메일을 입력하세요.' };
+      if (criteriaMode === 'firstError') {
+        setErrors(newErrors);
+        return;
+      }
+    }else if(emailExp.test(userInfo.email) === false){
+      newErrors.email = { message: '이메일 양식에 맞지 않습니다.' };
+      if (criteriaMode === 'firstError') {
+        setErrors(newErrors);
+        return;
+      }
+    }
+    
+    if(userInfo.cellphone.trim() === ''){
+      newErrors.cellphone = { message: '휴대폰 번호를 입력하세요.' };
+      if (criteriaMode === 'firstError') {
+        setErrors(newErrors);
+        return;
+      }
+    }else if(cellphoneExp.test(userInfo.cellphone) === false){
+      newErrors.cellphone = { message: '휴대폰 형식에 맞지 않습니다.' };
+      if (criteriaMode === 'firstError') {
+        setErrors(newErrors);
+        return;
+      }
+    }
+    
     setErrors(newErrors);
-      if(Object.keys(newErrors).length > 0){  
-        // 입력값 검증 실패
-        console.log('입력값 검증 실패', newErrors);
-      }else{  
-        // 입력값 검증 통과
-        console.log('서버에 전송...', user);
-        setUser({
-          name:'',
-          email:'',
-          cellphone:''
-        });
-      }
-    }
-  
 
-  function registMember(event: React.FormEvent){
-    event.preventDefault(); // 브라우저의 기본 동작 취소
-    validate({
+    return Object.keys(newErrors).length === 0;
+    
+    // if(Object.keys(newErrors).length > 0){  // 입력값 검증 실패
+    //   return false;
+    // }else{  // 입력값 검증 통과
+    //   return true;
+    // }
+  }
+  
+  function registMember(event: React.FormEvent) {
+    event.preventDefault(); // 브라우저의 기본 동작 취소(submit 동작 취소)
+    const isValid = validate({
       // criteriaMode: 'firstError', // 첫번째 발견된 에러만 포함(기본값)
-      criteriaMode: 'all',   // 모든 에러를 포함
+      criteriaMode: 'all', // 모든 에러를 포함
     });
+    if(isValid){
+      console.log('서버에 전송', user);
+      setUser({ name: '', email: '', cellphone: '010' });
+    }
   }
 
-  return(
-  <>
-    <h1>15 회원가입 입력값 상태 관리</h1>
+  return (
+    <>
+      <h1>15 회원가입 입력값 상태 관리</h1>
 
-    <form onSubmit={ registMember }>
-      <label htmlFor="name">이름</label>
-      <input id="name" name="name" value={ user.name } onChange={ handleNameChange }/><br/>
-      <div className="error-style">{ errors.name?.message }</div>
+      <form onSubmit={ registMember }>
+        <label htmlFor="name">이름</label>
+        <input id="name" name="name" value={ user.name } onChange={ handleChange } /><br />
+        <div className="error-style">{ errors.name?.message }</div>
 
-      <label htmlFor="email">이메일</label>
-      <input id="email" name="email" value={ user.email } onChange={ handleNameChange }/><br/>
-      <div className="error-style">{ errors.email?.message }</div>
+        <label htmlFor="email">이메일</label>
+        <input id="email" name="email" value={ user.email } onChange={ handleChange } /><br />
+        <div className="error-style">{ errors.email?.message }</div>
 
-      <label htmlFor="cellphone">휴대폰</label>
-      <input id="cellphone" name="cellphone" value={ user.cellphone } onChange={ handleNameChange }/><br/>
-      <div className="error-style">{ errors.cellphone?.message }</div>
+        <label htmlFor="cellphone">휴대폰</label>
+        <input id="cellphone" name="cellphone" value={ user.cellphone } onChange={ handleChange } /><br />
+        <div className="error-style">{ errors.cellphone?.message }</div>
 
-      <button type="submit">가입</button>
-    </form>
+        <button type="submit">가입</button>
+      </form>
 
-    <p>
-      이름: { user.name } <br/>
-      이메일: { user.email } <br/>
-      휴대폰: { user.cellphone } <br/>
-    </p>
-  </>
+      <p>
+        이름: { user.name }<br />
+        이메일: { user.email }<br />
+        휴대폰: { user.cellphone }<br />
+      </p>
+    </>
   );
 }
+
 export default App;
