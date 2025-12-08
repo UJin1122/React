111038d3198081f3ecf12e3621ea8dc6954bf3b5
kilyong jeong <uzoolove@gmail.com>
2025-12-08 23:54:27 +0900

    03 클래스 컴포넌트 - 컴포넌트의 라이프 사이클 완성

--- workspace-ins/ch03-class/03-lifecycle/src/App.tsx
+++ workspace-ins/ch03-class/03-lifecycle/src/App.tsx
@@ -1,15 +1,18 @@
 import ClickMe from "@/ClickMe";
-import { Component } from "react";
+import { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>03 클래스 컴포넌트 - 컴포넌트의 라이프 사이클</h1>
-        <ClickMe level={2} />
-      </div>
-    );
-  }
+function App() {
+  console.log('App 리렌더링.');
+  const [ show, setShow ] = useState<boolean>(true);
+
+  // 버튼 클릭 시 ClickMe 컴포넌트를 제거
+  return (
+    <div>
+      <h1>03 클래스 컴포넌트 - 컴포넌트의 라이프 사이클</h1>
+      <button onClick={() => setShow(false)}>ClickMe 제거</button>
+      { show ? <ClickMe level={2} /> : '' }
+    </div>
+  );
 }
 
 export default App;
