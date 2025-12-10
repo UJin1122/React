73f3953460afd9ef53f5de991b0a441b4b2c092c
kilyong jeong <uzoolove@gmail.com>
2025-12-10 13:19:04 +0900

    04 useRef - 값이 유지되는 데이터 관리, DOM 엘리먼트 참조 완성

--- workspace-ins/ch04-hooks/04-useRef/src/components/Counter.tsx
+++ workspace-ins/ch04-hooks/04-useRef/src/components/Counter.tsx
@@ -16,7 +16,7 @@ function Counter({ children }: CounterProps) {
   const stepRef = useRef(1); // { current: 1 } 객체를 반환
 
   // 2. DOM 객체에 대한 직접 참조를 사용할 때
-  const stepElem = useRef(null);
+  const stepElem = useRef<HTMLInputElement>(null); // { current: null } 객체를 반환
 
   // 카운트 감소
   const handleDown = () => {
@@ -33,14 +33,15 @@ function Counter({ children }: CounterProps) {
     setCount(initCount);
     // step input 요소에 포커스 지정
     // const stepElem = document.querySelector('#step') as HTMLInputElement;
-    // stepElem.focus();
 
+    stepElem.current?.focus();
   };
 
   return (
     <div id="counter">
       <label htmlFor="step">증감치</label>
       <input 
+        ref={ stepElem }
         id="step" 
         type="number" 
         defaultValue="1"
