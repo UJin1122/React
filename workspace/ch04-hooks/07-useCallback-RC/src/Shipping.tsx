bbc24f7d801289bb59150eba12d43494e834c4b4
UJin1122 <enooooo93@gmail.com>
2025-12-11 14:08:00 +0900

    4/7 4/8 기본세팅

---
+++ workspace/ch04-hooks/07-useCallback-RC/src/Shipping.tsx
@@ -0,0 +1,24 @@
+interface ShippingProps{
+  fees: number;
+  handlePayment: () => void;
+}
+
+function Shipping({ fees, handlePayment }:ShippingProps){
+  console.log('\tShipping 렌더링');
+
+  return(
+    <>
+    <h2>배송비</h2>
+    <div>
+    배송비: { fees.toLocaleString() }원<br />
+    </div>
+    <br />
+    {/* handlePayment 함수가 매번 App에서 생성됨. 메모이제이션 의미 없음 */}
+    <button type="button" onClick={ handlePayment }>결제</button>
+
+    </>
+  );
+}
+
+// 메모이제이션을 제대로 사용하기 위해서는 handlePayment 새로 생성이 안되게 해야 함
+export default Shipping;
---
+++ workspace/ch04-hooks/08-customCounter/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      // eslint가 node_modules를 기준으로 프로젝트 루트를 인식함
+      // 프로젝트에서 자체 node_modules 를 사용하지 않음
+      // 상위 폴더에서 tsconfig 파일을 여러개 찾아내기 때문에 오류 발생
+      // 프로젝트 루트를 명시해야 함
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname, // 현재 파일이 있는 디렉토리 경로
+      }
+    },
+  },
+])
