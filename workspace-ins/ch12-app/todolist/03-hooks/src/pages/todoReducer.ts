65d942bfdf1498cde93f22e1b47420fa168d3a83
kilyong jeong <uzoolove@gmail.com>
2025-12-11 10:26:36 +0900

    4. immer 라이브러리를 이용해서 상태의 불변성을 유지하도록 수정

--- workspace-ins/ch12-app/todolist/03-hooks/src/pages/todoReducer.ts
+++ workspace-ins/ch12-app/todolist/03-hooks/src/pages/todoReducer.ts
@@ -1,22 +1,51 @@
 import type { TodoItem } from "@/pages/TodoItem";
+import { produce } from "immer";
 
 type TodoAction = 
   | { type: 'ADD'; value: TodoItem }
   | { type: 'TOGGLE' | 'DELETE'; value: Pick<TodoItem, '_id'> };
 
 // TODO 3. 상태관리 로직을 작성하고 TodoContainer 컴포넌트에서 useState대신 useReducer를 사용하도록 수정
+// function todoReducer(state: TodoItem[], action: TodoAction) {
+//   let newState;
+
+//   switch(action.type){
+//     case 'ADD':
+//       newState = [ ...state, action.value ];
+//       break;
+//     case 'TOGGLE':
+//       newState = state.map(item => item._id === action.value._id ? { ...item, done: !item.done } : item);
+//       break;
+//     case 'DELETE':
+//       newState = state.filter(item => item._id !== action.value._id );
+//       break;
+//     default:
+//       newState = state;
+//   }
+
+//   return newState;
+// }
+
+// TODO 4. immer 라이브러리를 이용해서 상태의 불변성을 유지하도록 수정
 function todoReducer(state: TodoItem[], action: TodoAction) {
   let newState;
+  const targetIndex = state.findIndex(item => item._id === action.value._id);
 
   switch(action.type){
     case 'ADD':
-      newState = [ ...state, action.value ];
+      newState = produce(state, (draft) => {
+        draft.push(action.value);
+      });
       break;
     case 'TOGGLE':
-      newState = state.map(item => item._id === action.value._id ? { ...item, done: !item.done } : item);
+      newState = produce(state, (draft) => {
+        draft[targetIndex].done = !draft[targetIndex].done;
+      });
       break;
     case 'DELETE':
-      newState = state.filter(item => item._id !== action.value._id );
+      newState = produce(state, (draft) => {
+        draft.splice(targetIndex, 1);
+      });
       break;
     default:
       newState = state;
@@ -24,10 +53,5 @@ function todoReducer(state: TodoItem[], action: TodoAction) {
 
   return newState;
 }
-
-// TODO 4. immer 라이브러리를 이용해서 상태의 불변성을 유지하도록 수정
-// function todoReducer(state: TodoItem[], action: TodoAction) {
-  
-// }
 
 export default todoReducer;
