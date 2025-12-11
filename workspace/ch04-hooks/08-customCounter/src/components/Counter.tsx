3a7490eb8d18efb796d9dae5eaf2ddad04f7c9da
UJin1122 <enooooo93@gmail.com>
2025-12-11 14:08:00 +0900

    4/7 4/8 기본세팅

---
+++ workspace/ch04-hooks/08-customCounter/src/components/Counter.tsx
@@ -0,0 +1,55 @@
+import Button from "@/components/Button";
+import { useState } from "react";
+
+interface CounterProps{
+  children: string;
+}
+
+function Counter({ children }: CounterProps){
+  console.log('\tCounter 호출됨.');
+
+  const initCount = Number(children);
+  
+  const [ count, setCount ] = useState(initCount);
+  const [ change, setChange ] = useState(1);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setChange(Number(e.target.value));  
+  };
+  
+  // 카운터 감소
+  const handleDown = () => {
+    setCount(count - change);
+  };
+
+  // 카운터 증가
+  const handleUp = () => {
+    setCount(count + change);
+  };
+
+  // 카운터 초기화
+  const handleReset = () => {
+    setCount(initCount);
+  };
+
+  return(
+    <>
+    <div id="counter">
+    <label htmlFor="change" style={{fontSize:"20px"}}>증감치</label>
+    <input style={{ padding:8, borderRadius:6, border:"solid 1px" }} 
+      onChange={ handleChange }
+      id="change" 
+      type="number" 
+      value={ change }
+    />
+    <div style={{display:"flex", alignContent:"center", justifyContent:"center", padding:"8px"}}>
+    <Button type={ "button" } onClick={ handleDown }>-_-</Button>
+    <Button bgColor="gray" style={{color:'black'}} type={ "button" } onClick={ handleReset }>0_0</Button>
+    <Button type={ "button" } onClick={ handleUp }>+_+</Button>
+    </div>
+    <span style={{display:"flex", alignContent:"center", justifyContent:"center"}}>{ count }</span>
+    </div>
+    </>
+  );
+}
+export default Counter;
