e5facb7c985bf85400c394d2df4511946713cfc0
UJin1122 <enooooo93@gmail.com>
2025-12-12 13:14:52 +0900

    .

--- workspace/ch04-hooks/10-customFetch/src/hooks/useFetch.ts
+++ workspace/ch04-hooks/10-customFetch/src/hooks/useFetch.ts
@@ -3,56 +3,55 @@ import { useEffect, useState } from "react";
 
 const API_SERVER = 'https://fesp-api.koyeb.app/todo';
 
-interface FetchParams{
+interface FetchParams {
   url: string;
-  method?: string;
+  method?: 'GET' | 'POST';
 }
 
-function useFetch(fetchParams: FetchParams){
+function useFetch(fetchParams: FetchParams) {
+  // Todo 목록을 저장할 상태 (초기값: null)
+  const [ data, setData ] = useState<TodoListRes | null>(null);
 
-// Todo 목록을 저장할 상태 (초기값: null)
-const [ data, setData ] = useState<TodoListRes | null>(null);
+  // 에러 메시지를 저장할 상태 (초기값: null)
+  const [ error, setError ] = useState<ErrorRes | null>(null);
 
-// 에러 메세지를 저장할 상태 (초기값: null)
-const [ error, setError ] = useState<ErrorRes | null>(null);
+  // 로딩 상태를 저장 (초기값: true)
+  const [ loading, setLoading ] = useState(true);
 
-// 로딩 상태를 저장 (초기값: true)
-const [ loading, setLoading ] = useState(true);
+  const requestFetch = async (params: FetchParams) => {
+    try{
+      // 로딩중 상태 표시
+      setLoading(true);
 
-const requestFetch = async (url) => {
-  try {
-    setLoading(true);
+      const res = await fetch(API_SERVER + params.url);
+      console.log('res', res);
 
-    const res = await fetch(API_SERVER + url);
-    console.log('res', res);
+      const jsonRes: ResData = await res.json();
+      console.log('body', jsonRes);
 
-    const jsonRes: ResData = await res.json();
-    console.log('body', jsonRes);
-    
-    if(jsonRes.ok === 1){ // 타입 가드
-    setData(jsonRes);
-    setError(null);
-    }else{ 
-      // API 서버에서 에러를 응답받을 경우
-      throw new Error(jsonRes.message);
+      if(jsonRes.ok === 1){ // 타입 가드
+        setData(jsonRes);
+        setError(null);
+      }else{
+        // API 서버에서 에러를 응답 받을 경우
+        throw new Error(jsonRes.message);
+      }
+    }catch(err){
+      // 네트워크 오류 같은 에러 발생 시
+      setError(err as ErrorRes);
+      setData(null);
+    }finally{
+      // 성공, 실패 여부와 관계없이 로딩 상태를 false로 설정
+      setLoading(false);
     }
-    
-  }catch (error) {
-    // 네트워크 오류, API서버 오류
-    setError(error as ErrorRes);
-    setData(null);
-  }finally{
-    // 성공, 실패 여부와 상관없이 로딩 상태를 false로 설정
-    setLoading(false);
-  }
-};
+  };
 
-// 컴포넌트가 마운트 된 후에 실행
-useEffect(()=>{
-  requestFetch(fetchParams);
-},[]);
+  // 컴포넌트가 마운트 된 후에 실행
+  useEffect(() => {
+    requestFetch(fetchParams);
+  }, []); // 빈 배열을 전달해서 마운트시 한번만 호출되도록 설정
 
-return { data, error, loading};
-
+  return { data, error, loading };
 }
+
 export default useFetch;
--- workspace/ch04-hooks/10-customFetch/src/pages/TodoList.tsx
+++ workspace/ch04-hooks/10-customFetch/src/pages/TodoList.tsx
@@ -3,22 +3,23 @@ import useFetch from "@/hooks/useFetch";
 function TodoList(){
 
   const { data, error, loading } = useFetch({url:'/todolist'});
+
+  const list = data?.items.map(item => <li key={ item._id }>{ item.title }</li>)
+
   return(
     <>
     <h1>10 customHook - useFatch, useAxios 커스텀 훅 사용</h1>
     <h2>할일 목록</h2>
 
     {/* 로딩중일 때 로딩중 메시지 표시 */}
-    <p>로딩중...</p> 
+    { loading && <p>로딩중...</p> } 
   
     {/* 에러가 있을 경우 빨간색으로 에러 메시지 표시 */}
-    <p style={{color: "red"}}>네트워크 연결 오류</p> 
+    { error && <p style={{color: "red"}}>네트워크 연결 오류</p> } 
       
     {/* 서버에서 받은 Todo 목록을 렌더링 */}
     <ul>
-      <li>React 학습</li>
-      <li>Next.js 학습</li>
-      <li>파이널 프로젝트</li>
+      { data && list}
     </ul>
     </>
   );
