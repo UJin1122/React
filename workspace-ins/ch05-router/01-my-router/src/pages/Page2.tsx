27698740af94d964e0fc5ad76d144fd4dbd78dc5
kilyong jeong <uzoolove@gmail.com>
2025-12-16 13:21:57 +0900

    리액트 라우터 - 01 클라이언트 라우팅 직접 구현 작성중...

--- workspace-ins/ch05-router/01-my-router/src/pages/Page2.tsx
+++ workspace-ins/ch05-router/01-my-router/src/pages/Page2.tsx
@@ -1,7 +1,16 @@
 function Page2() {
+  console.log('Page2 렌더링');
   return (
     <>
-      
+      <header>
+        <h1>리액트 라우터 - 01 클라이언트 라우팅 직접 구현</h1>
+        <a className="menu" href="home.html">home</a>
+        <br/>
+        <a className="menu" href="page1.html">page1</a>
+        <br/>
+        <a className="menu-dark" href="page2.html">page2</a>
+      </header>
+      <h2>SPA Page2</h2>
     </>
   )
 }
