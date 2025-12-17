ca9f3118f3351acba84e41a12c0e1ff17af66ec4
UJin1122 <enooooo93@gmail.com>
2025-12-17 09:11:11 +0900

    5-2

--- workspace/ch05-router/02-react-router/src/pages/Home.tsx
+++ workspace/ch05-router/02-react-router/src/pages/Home.tsx
@@ -1,12 +1,13 @@
-import Header from "@components/Header";
+import Header from "@components/Header.tsx";
 
 function Home() {
+  console.log('Home 렌더링');
   return (
-    <>
-      <Header />
-      <h2>SPA Home</h2>
-    </>
-  );
+  <>
+    <Header/>
+    <h2>SPA Home</h2> 
+  </>
+  )
 }
 
 export default Home;
