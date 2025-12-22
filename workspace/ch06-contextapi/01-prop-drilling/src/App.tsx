1896a4dfb1db9d705024c6fb9ee9784bcebf0e89
UJin1122 <enooooo93@gmail.com>
2025-12-22 15:28:16 +0900

    6-1

--- workspace/ch06-contextapi/01-prop-drilling/src/App.tsx
+++ workspace/ch06-contextapi/01-prop-drilling/src/App.tsx
@@ -1,9 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Left1 from '@/components/Left1';
 import Right1 from '@/components/Right1';
 import './App.css';
+import { CounterProvider } from '@/contexts/CounterContext';
 
 function App() {
+
+  const [ count,setCount ] = useState(0);
+
+  const countUp = (step:number) => {
+    setCount(count + step);
+  };
 
   useEffect(()=>{
     console.log('# App 렌더링.');
@@ -15,8 +22,11 @@ function App() {
       <div id="container">
         <h1>App</h1>
         <div id="grid">
-          <Left1 />
-          <Right1 />
+          <CounterProvider>
+            <Left1 count={ count }/>
+            <Right1 countUp={ countUp }/>
+          </CounterProvider>
+          
         </div>
       </div>
     </>
