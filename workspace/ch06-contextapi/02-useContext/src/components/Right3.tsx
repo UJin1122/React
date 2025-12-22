1d42a18dcec04e23ce617a06dd536c1ca8905c1a
UJin1122 <enooooo93@gmail.com>
2025-12-22 16:09:29 +0900

    6-2

--- workspace/ch06-contextapi/02-useContext/src/components/Right3.tsx
+++ workspace/ch06-contextapi/02-useContext/src/components/Right3.tsx
@@ -1,13 +1,19 @@
-import { useEffect } from 'react';
+import CounterContext from '@/contexts/CounterContext';
+import { useContext, useEffect } from 'react';
 
 function Right3() {
   useEffect(()=>{
     console.log('#### Right3 렌더링.');
   });
+  
+  const counter = useContext(CounterContext);
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button style={{ borderRadius:'7px', padding:'5px', marginRight:'5px'}} onClick={ () => { counter?.countUp(1) } }>+1</button>
+      <button style={{ borderRadius:'7px', padding:'5px', marginRight:'5px'}} onClick={ () => { counter?.reset(0) } }>0</button>
+      <button style={{ borderRadius:'7px', padding:'5px', marginRight:'5px'}} onClick={ () => { counter?.countDown(1) } }>-1</button>
     </div>
   );
 }
