f585ca84732d4a173add3345d0cd22d86498df61
UJin1122 <enooooo93@gmail.com>
2025-12-24 16:30:34 +0900

    8-4 완

--- workspace/ch08-css/04-tailwind/src/components/ui/Button.tsx
+++ workspace/ch08-css/04-tailwind/src/components/ui/Button.tsx
@@ -1,13 +1,44 @@
-import styles from './Button.module.css';
+const btnBg = {
+  gray: 'bg-gray-500',
+  blue: 'bg-blue-500',
+  red: 'bg-red-500',
+  white: 'bg-white-500',
+} as const; // 모든 속성 Readonly/ 각 속성의 타입이 리터럴로 추론(stringX)
+
+const btnColor = {
+  black: 'text-black',
+  red: 'text-red',
+  blue: 'text-blue',
+  white: 'text-white',
+} as const;
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
-  color?: string;
-  bg?: string;
+  // bg?: 'gray' | 'blue' | 'red' | 'white';
+  // btnBg의 타입의 키들을 유니온으로 반환
+  bg?: keyof typeof btnBg;
+  color?: keyof typeof btnColor;
 }
 
-function Button({ children, type='button', bg, color, ...rest }: ButtonProps){
+function Button({ children, type='button', bg='gray', color='black', ...rest }: ButtonProps){
   return (
-    <button type={ type } className={`${styles.button} ${styles[`bg-${bg}-text-${color}`]}`} { ...rest }>{ children }</button>
+    <button type={ type } 
+    className={`
+      ${btnBg[bg]} 
+      border 
+      border-black/30
+      rounded-md
+      text-center
+      no-underline
+      py-2
+      px-4
+      my-1
+      mx-0.5
+      inline-block
+      text-base
+      ${btnColor[color]}`}
+    { ...rest }>
+    { children }
+    </button>
   );
 }
 
