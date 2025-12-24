4e1aa32f118d902e566c46bf2418cf0bf82c87b4
kilyong jeong <uzoolove@gmail.com>
2025-12-24 16:44:53 +0900

    tailwind css 완성

--- workspace-ins/ch08-css/04-tailwind/src/App.tsx
+++ workspace-ins/ch08-css/04-tailwind/src/App.tsx
@@ -3,11 +3,11 @@ import Button from "@components/ui/Button";
 function App(){
   return (
     <>
-      <h1>04 Tailwind CSS</h1>
+      <h1 className="text-[2em] font-bold my-[0.67em] block">04 Tailwind CSS</h1>
 
       <Button>그냥 버튼</Button>
-      <Button type="reset" bg="blue" color="red">취소 버튼</Button>
-      <Button type="submit" bg="gray" color="blue">submit 버튼</Button>
+      <Button type="reset" bg="red" color="white">취소 버튼</Button>
+      <Button type="submit" bg="blue" color="white">submit 버튼</Button>
 
     </>
   );
