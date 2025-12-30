24c24e1bcda005b4caf06d83758eb2305c6e745a
kilyong jeong <uzoolove@gmail.com>
2025-12-30 09:38:27 +0900

    TODO 2: 타입 추가

--- workspace-ins/ch09-ajax/01-board-fetch/src/types/board.ts
+++ workspace-ins/ch09-ajax/01-board-fetch/src/types/board.ts
@@ -21,6 +21,24 @@ export interface BoardInfoRes {
   item: BoardInfo;
 }
 
+// 댓글 타입
+export interface BoardReply {
+  _id: number;
+  content: string;
+}
+
+// 댓글 목록 조회 결과 타입
+export interface BoardReplyListRes {
+  ok: 1;
+  item: BoardReply[];
+}
+
+// 댓글 등록 결과 타입
+export interface BoardReplyCreateRes {
+  ok: 1;
+  item: BoardReply;
+}
+
 // 서버에서 에러를 응답할 경우
 export interface ErrorRes {
   ok: 0;
@@ -28,4 +46,4 @@ export interface ErrorRes {
 }
 
 // 서버의 응답
-export type ResData<T extends BoardListRes | BoardInfoRes> = T | ErrorRes;
+export type ResData<T extends BoardListRes | BoardInfoRes | BoardReplyListRes | BoardReplyCreateRes> = T | ErrorRes;
