54e15f0568617702ec6e9fc28270dfe200f7ecfb
UJin1122 <enooooo93@gmail.com>
2025-12-31 15:30:33 +0900

    9-4

--- workspace/ch09-ajax/04-board-pattern/src/PostList.tsx
+++ workspace/ch09-ajax/04-board-pattern/src/PostList.tsx
@@ -1,23 +1,37 @@
-import FetchOnRender from "@/FetchOnRender";
-// import FetchThenRender from "@/FetchThenRender";
+// import FetchOnRender from "@/FetchOnRender";
+import type { BoardList, BoardListRes } from "@/types/board";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import FetchThenRender from "@/FetchThenRender";
 
 // 게시물 목록 조회 API 호출
 function fetchList(){
-  
+  return axios.get<BoardListRes>('https://fesp-api.koyeb.app/market/posts?delay=4000',{
+    headers:{ 'client-Id': 'openmarket' }
+  }).then((response) => response.data.item);
 }
 
 function PostList(){
   // TODO: 게시물 목록 조회
+  const [ data, setData ] = useState<BoardList[]>();
 
+  useEffect(()=>{
+    fetchList().then(setData);
+  },[]);
+  // 데이터가 준비되기 전의 대체 컨텐츠
+  if(!data){
+    return <h2>게시물 목록 로딩중...</h2>;
+  }
+  // 데이터가 준비된 이후에 렌더링 됨
   return (
     <>
-      <h2>게시물 5건.</h2>
+      <h2>게시물 { data?.length }건</h2>
+      {/* <hr /> */}
+      {/* <h3>Fetch-On-Render 방식(폭포수 현상)</h3>
+      <FetchOnRender /> */}
       <hr />
-      <h3>Fetch-On-Render 방식(폭포수 현상)</h3>
-      <FetchOnRender />
-      {/* <hr />
       <h3>Fetch-Then-Render 방식(복잡도 증가)</h3>
-      <FetchThenRender /> */}
+      <FetchThenRender />
     </>
   );
 }
